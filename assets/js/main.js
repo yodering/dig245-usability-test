559d5be18cf238b7545ab431a7c87fe56e82da24
David Yoder <david.yoder@live.com>
2023-10-18 14:14:41 -0400

    added sus test text

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,24 @@
 
 /* javascript */
+
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("button");
+
+    button.addEventListener("click", function() {
+        let q1 = parseInt(document.getElementById('q1').value);
+        let q2 = parseInt(document.getElementById('q2').value);
+        let q3 = parseInt(document.getElementById('q3').value);
+        let q4 = parseInt(document.getElementById('q4').value);
+        let q5 = parseInt(document.getElementById('q5').value);
+        let q6 = parseInt(document.getElementById('q6').value);
+        let q7 = parseInt(document.getElementById('q7').value);
+        let q8 = parseInt(document.getElementById('q8').value);
+        let q9 = parseInt(document.getElementById('q9').value);
+        let q10 = parseInt(document.getElementById('q10').value);
+
+        let susScore = (q1-1+5-q2+q3-1+5-q4+q5-1+5-q6+q7-1+5-q8+q9-1+5-q10) * 2.5
+
+        const scoreValue = document.getElementById("scoreValue");
+        scoreValue.textContent = susScore;
+    })
+})
