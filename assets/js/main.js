ff491fccb09a94a6e8282be1482c5e664cb50743
yodering <david.yoder@live.com>
2023-10-18 21:17:31 -0400

    added js to the sus

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,34 @@
 
 /* javascript */
 
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.getElementById("button");
+let myForm = document.querySelector("#sus");
 
-    button.addEventListener("click", function() {
-        let q1 = parseInt(document.getElementById('q1').value);
-        let q2 = parseInt(document.getElementById('q2').value);
-        let q3 = parseInt(document.getElementById('q3').value);
-        let q4 = parseInt(document.getElementById('q4').value);
-        let q5 = parseInt(document.getElementById('q5').value);
-        let q6 = parseInt(document.getElementById('q6').value);
-        let q7 = parseInt(document.getElementById('q7').value);
-        let q8 = parseInt(document.getElementById('q8').value);
-        let q9 = parseInt(document.getElementById('q9').value);
-        let q10 = parseInt(document.getElementById('q10').value);
+myForm.addEventListener("submit", (event) => {
 
-        let susScore = (q1-1+5-q2+q3-1+5-q4+q5-1+5-q6+q7-1+5-q8+q9-1+5-q10) * 2.5
+	event.preventDefault();
 
-        const scoreValue = document.getElementById("scoreValue");
-        scoreValue.textContent = susScore;
-    })
-})
+	let q1 = this.q1.value;
+	let q2 = this.q2.value;
+	let q3 = this.q3.value;
+	let q4 = this.q4.value;
+	let q5 = this.q5.value;
+	let q6 = this.q6.value;
+	let q7 = this.q7.value;
+	let q8 = this.q8.value;
+	let q9 = this.q9.value;
+	let q10 = this.q10.value;
+
+	q1 -= 1;
+	q3 -= 1;
+	q5 -= 1;
+	q7 -= 1;
+	q9 -= 1;
+	q2 = 5 - q2;
+	q4 = 5 - q4;
+	q6 = 5 - q6;
+	q8 = 5 - q8;
+	q10 = 5 - q10;
+
+
+	this.result.value = (q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 + q10) * 2.5;;
+});
